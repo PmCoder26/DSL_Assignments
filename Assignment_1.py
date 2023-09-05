c1c233594ec0f10cfd4095eba4dcb02f5fb501e2
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2023-09-05 16:00:19 +0530

    Update Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,22 +1,29 @@
-# Assignment - 1.
-'''
-        Question: In second year computer engineering class, group A student’s play cricket, group B students play badminton and group C students play football.
-        Write a Python program using functions to compute following: -
-            a)List of students who play both cricket and badminton
-            b)List of students who play either cricket or badminton but not both
-            c)Number of students who play neither cricket nor badminton
-            d)Number of students who play cricket and football but not badminton.
-'''
+# Assignment-1.
 
+"""
+    In second year computer engineering class, group A student’s play cricket, group B
+    students play badminton and group C students play football.
+    Write a Python program using functions to compute following: -
+            a) List of students who play both cricket and badminton
+            b) List of students who play either cricket or badminton but not both
+            c) Number of students who play neither cricket nor badminton
+            d) Number of students who play cricket and football but not badminton.
+"""
 
-def union(list4, list5):  # Calculating the union of two lists.
+def createList(list):
+    n=int(input("Enter the no.of players: "))
+    for i in range(0, n):
+        list.append(input("Name: "))
+    return list
+
+def union(list4, list5):
     for i in range(0, len(list5)):
         if list5[i] not in list4:
             list4.append(list5[i])
     return list4
 
 
-def intersection(list4, list5):  # Calculating the intersection of two lists.
+def intersection(list4, list5):
     list6 = []
     for i in range(0, len(list5)):
         if list5[i] in list4:
@@ -24,7 +31,7 @@ def intersection(list4, list5):  # Calculating the intersection of two lists.
     return list6
 
 
-def nCnB(list4, list5, list6):  # Finding the players who neither play cricket nor badminton.
+def nCnB(list4, list5, list6):
     list7 = []
     for i in range(0, len(list6)):
         if list6[i] not in list4 and list6[i] not in list5:
@@ -32,7 +39,7 @@ def nCnB(list4, list5, list6):  # Finding the players who neither play cricket n
     return list7
 
 
-def CAndFNotB(list1, list2, list3):  # Finding the players who play cricket adn football but not badminton.
+def CAndFNotB(list1, list2, list3):
     list4 = intersection(list1, list3)
     list5 = []
     for i in range(0, len(list4)):
@@ -41,15 +48,20 @@ def CAndFNotB(list1, list2, list3):  # Finding the players who play cricket adn 
     return list5
 
 
-# ---------------------------------------------------------------------------------------------------------------------------->
+listA = []  # Cricket players.
+listB = []  # Badminton players.
+listC = []  # Football players.
 
-listA = ["P1", "P2", "P3", "P4", "P9", "P5"]  # Cricket players.
-listB = ["P1", "P2", "P6", "P7", "P8", "P4"]  # Badminton players.
-listC = ["P8", "P10", "P11", "P4", "P5"]  # Football players.
+# creating the lists of players via user input.
+print("Creating list of players playing Cricket-")
+listA = createList(listA)
+print("Creating list of players playing Badminton-")
+listB = createList(listB)
+print("Creating list of players playing Football-")
+listC = createList(listC)
 
-print("The students who play both cricket and badminton are:", intersection(listA, listB))
 print("The students who play either cricket or badminton but not both are:", union(listA, listB))
-print("The students who play neither cricket not badminton are:", nCnB(listA, listB, listC))
+print("The students who play both cricket and badminton are:", intersection(listA, listB))
 print("Number of students who play neither cricket nor badminton is:", len(nCnB(listA, listB, listC)))
-print("The students who play cricket and football but not badminton is:", CAndFNotB(listA, listB, listC))
+print(nCnB(listA, listB, listC))
 print("Number of students who play cricket and football but not badminton is:", len(CAndFNotB(listA, listB, listC)))
